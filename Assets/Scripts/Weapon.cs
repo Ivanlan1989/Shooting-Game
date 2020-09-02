eba55025265c563579c782a5761c42ba021c8650
Ivan <drift7ltd@hotmail.com>
2020-09-02 23:26:45 +1200

    Add Game Project

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject[] loadout;
+    public Transform weaponParent;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
+    }
+
+    void Equip (int p_ind)
+    {
+        GameObject t_newEquipment = Instantiate(loadout[p_ind], weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+        t_newEquipment.transform.localPosition = Vector3.zero;
+        t_newEquipment.transform.localEulerAngles = Vector3.zero;
+    }
+}
