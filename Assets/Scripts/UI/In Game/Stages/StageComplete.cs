8bb3944dd9da9b1f6e7c7be5f5d7af53b49b7974
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2020-10-01 11:40:41 +1300

    Added stages
    When you complete a stage you will see a message in the Unity console.

---
+++ Assets/Scripts/UI/In Game/Stages/StageComplete.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class StageComplete : MonoBehaviour
+{
+    #region Variables
+    #region Public
+    public GameObject player;
+    public string message;
+    #endregion
+    #region Private
+    private bool isStageComplete;
+    #endregion
+    #endregion
+    #region Collision Callbacks
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (!isStageComplete)
+            {
+                Debug.Log(message);
+                isStageComplete = true;
+            }
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    public bool IsStageComplete()
+    {
+        return isStageComplete;
+    }
+    #endregion
+}
