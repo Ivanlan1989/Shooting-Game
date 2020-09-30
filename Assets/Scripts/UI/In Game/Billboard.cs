90fc40d36fdc4817d53a9251a0b3779b28c4dae5
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2020-10-01 11:28:32 +1300

    Added a guide animation for the player
    Added in a simple animation on some text which acts as a guide.

---
+++ Assets/Scripts/UI/In Game/Billboard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    #region Variables
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+    public Transform camera;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+    #endregion
+
+    void LateUpdate()
+    {
+        if (camera == null)
+        {
+            GameObject[] tmpGOS = FindObjectsOfType<GameObject>();
+            foreach (GameObject tmpGO in tmpGOS)
+            {
+                if (tmpGO.name == "Normal Camera")
+                {
+                    camera = tmpGO.transform;
+                    Debug.Log("Found " + camera.name);
+                    break;
+                }
+            }
+        }
+        if (camera != null)
+        {
+            transform.LookAt(transform.position + camera.forward);
+        }
+    }
+}
