77ec440aac3fbb95b53d0cf794af4e0a4ed542a5
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2020-10-08 13:13:33 +1300

    Fixed an issue with the git cloning

---
+++ Assets/Scripts/UI/In Game/Stages/FinalStageComplete.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalStageComplete : MonoBehaviour
+{
+    #region Variables
+    #region Public
+    public GameObject player;
+    public string stageCompletedMessage;
+    public Text stageCompletedText;
+    public GameObject stageCompletedGO;
+    public float stageCompletedMessageDuration = 4f;
+    public GameObject levelLoader;
+    #endregion
+    #region Private
+    private bool isStageComplete;
+    #endregion
+    #endregion
+    #region Collision Callbacks
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (!isStageComplete)
+            {
+                StartCoroutine(StageCompletedUIMessage());
+                isStageComplete = true;
+                LoadNextLevel();
+            }
+        }
+    }
+
+    private Text FindMissingTextObject(string textObjectName)
+    {
+        Text[] tmpGOS = FindObjectsOfType<Text>();
+        Text tmpFoundText = null;
+        foreach (Text tmpTextObject in tmpGOS)
+        {
+            if (tmpTextObject.name == textObjectName)
+            {
+                tmpFoundText = tmpTextObject;   
+                break;
+            }
+        }
+        return tmpFoundText;
+    }
+
+    private GameObject FindMissingGameObject(string gameObjectName)
+    {
+        GameObject[] tmpGOS = FindObjectsOfType<GameObject>();
+        GameObject tmpFoundGO = null;
+        foreach (GameObject tmpGO in tmpGOS)
+        {
+            if (tmpGO.name == gameObjectName)
+            {
+                tmpFoundGO = tmpGO;
+                break;
+            }
+        }
+        return null;
+    }
+
+    private IEnumerator StageCompletedUIMessage()
+    {
+        Debug.Log(stageCompletedMessage);
+        if (stageCompletedText == null)
+        {
+            stageCompletedText = FindMissingTextObject("Stage Completed");
+        }
+        if (stageCompletedText == null)
+        {
+            stageCompletedGO = FindMissingGameObject("Stage Completed");
+        }
+        stageCompletedText.text = stageCompletedMessage;
+        stageCompletedGO.SetActive(true);
+        yield return new WaitForSeconds(stageCompletedMessageDuration);
+        stageCompletedGO.SetActive(false);
+    }
+
+    private void LoadNextLevel()
+    {
+        LevelLoader tmpLevelLoader = levelLoader.GetComponent<LevelLoader>();
+        
+    }
+    #endregion
+
+    #region Public Methods
+    public bool IsStageComplete()
+    {
+        return isStageComplete;
+    }
+    #endregion
+}
