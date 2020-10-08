0100bf736af67b6ca16b7cffe1414cc579f67aeb
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2020-10-08 16:48:26 +1300

    Single player improvements
    Added in a finish for after all the levels have been completed. It will then go back to the Main Menue after 6 seconds, this can be adjusted.

---
+++ Assets/Scripts/UI/AllSinglePlayerLevelsCompleted.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AllSinglePlayerLevelsCompleted : MonoBehaviour
+{
+	#region Variables
+	#region Public Variables
+	public GameObject loadingScreen;
+	public GameObject levelLoader;
+	public int nextScene;
+	public float delay = 6f;
+	public ResetMouseAndCursor resetMouse;
+	#endregion
+	#region Private Variables
+
+	#endregion
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Private Methods
+	#region MonoBehaviour Methods
+	private void Start()
+    {
+		StartCoroutine(Delay());
+    }
+	#endregion
+	private IEnumerator Delay()
+    {
+		yield return new WaitForSeconds(delay);
+		LoadNextLevel();
+    }
+
+	private void LoadNextLevel()
+	{
+		try
+		{
+			loadingScreen.SetActive(true);
+			levelLoader.GetComponent<LevelLoader>().LoadLevel(nextScene);
+			resetMouse.ResetMouse();
+		}
+		catch (Exception ex)
+		{
+			Debug.LogError("Error " + ex.Message + "\n" + ex.StackTrace);
+		}
+	}
+	#endregion
+}
---
+++ Assets/Scripts/UI/ResetMouseAndCursor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class ResetMouseAndCursor : MonoBehaviour
+{
+	#region Variables
+	#region Public Variables
+	public bool onStart = false;
+	#endregion
+	#region Private Variables
+	
+	#endregion
+	#endregion
+	
+	#region Public Methods
+	public void ResetMouse()
+    {
+		try
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		catch (Exception ex)
+		{
+			Debug.LogError("Error " + ex.Message + "\n" + ex.StackTrace);
+		}
+	}
+	#endregion
+	
+	#region Private Methods
+	#region MonoBehaviour Methods
+    private void Start()
+    {
+		if (onStart)
+		{
+			ResetMouse();
+		}
+    }
+	#endregion
+	
+	#endregion
+}
