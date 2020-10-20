d583371d284aaf5ca04036f120f9700feae6081f
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2020-10-21 09:14:18 +1300

    Created the video intro scene
    I have created the video intro scene and loaded in the intro video. It will then go on to the main menu once the video has been completed.

---
+++ Assets/Scripts/UI/Video Introduction/LoadMainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*Author: Robert Ankersmit
+ * I wrote this all myself, however I did take some inspiration from a tutorial video.
+ * I had a look at the tutorial to get an idea of how to create an introduction with a video player.
+ * However I wrote all the code myself, in my own way. As stated, I simply took the insperation and extrapolated the overall concept and made it myself.
+ * URL: https://www.youtube.com/watch?v=2kLQ8gq9xeQ&t=557s
+ */
+
+public class LoadMainMenu : MonoBehaviour
+{
+	#region Variables
+	#region Public Variables
+	public string sceneToLoad;
+	public float videoLength;
+	#endregion
+	#region Private Variables
+	private bool canLoadNextScene = false;
+	#endregion
+	#endregion
+	
+	#region Public Methods
+	
+	#endregion
+	
+	#region Private Methods
+	#region MonoBehaviour Methods
+    private void Start()
+    {
+		StartCoroutine(WaitForVideoToFinishPlaying());
+    }
+	#endregion
+	private void LoadNewScene()
+    {
+        try
+        {
+			SceneManager.LoadScene(sceneToLoad);
+        }
+		catch (Exception ex)
+        {
+			Debug.LogError("Error in loading new scene -> " + ex.Message + "\n" + ex.StackTrace);
+        }
+    }
+
+	private IEnumerator WaitForVideoToFinishPlaying()
+    {
+		yield return new WaitForSeconds(videoLength);
+		LoadNewScene();
+    }
+	#endregion
+}
